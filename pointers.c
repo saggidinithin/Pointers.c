946cd12455699b22e1b2947952aeb5514f2cd09b
saggidinithin <saggidinithin@gmail.com>
2026-02-11 22:58:03 +0530

    Add files via upload

---
+++ pointers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int *p;   
+
+    p = &a;   
+
+    printf("Value of a = %d\n", a);
+    printf("Address of a = %p\n", &a);
+    printf("Value of p (address of a) = %p\n", p);
+    printf("Value at pointer (*p) = %d\n", *p);
+
+    return 0;
+}
